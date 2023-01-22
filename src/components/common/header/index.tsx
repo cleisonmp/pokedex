6121274e4254588691592368982335d0636e3296
cleisonmp <cleison.mp@gmail.com>
2023-01-22 14:42:17 -0300

    feat: sticky header to top of the page :sparkles:

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   const totalPokemonsCatched = usePokedexStore((state) => state.pokemons.length)
 
   return (
-    <header className='flex items-center justify-center gap-4 py-6 text-xl'>
+    <header className='flex items-center justify-center gap-4 py-6 text-xl sticky top-0 z-9999 bg-gray-400'>
       <Link
         href='/'
         className='flex items-center gap-1 border-2 border-gray-700 p-1 transition-all hover:underline hover:brightness-125'
