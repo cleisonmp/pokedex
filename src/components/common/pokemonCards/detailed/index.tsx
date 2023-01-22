12cddd2b00f22f6037847202bce852d44d8498dc
cleisonmp <cleison.mp@gmail.com>
2023-01-22 11:59:13 -0300

    feat: sort abilities by slot :sparkles:

--- src/components/common/pokemonCards/detailed/index.tsx
+++ src/components/common/pokemonCards/detailed/index.tsx
@@ -116,17 +116,19 @@ export const DetailCard = ({
         return await axios
           .get<AbilityDetail>(ability.ability.url)
           .then((response) => {
-            return response.data
+            return { ...response.data, slot: ability.slot }
           })
       })
       const abilitiesData = await Promise.all(abilitiesPromisses)
 
-      return abilitiesData.map(({ id, name, effect_entries }) => {
-        const effect =
-          effect_entries.find((entry) => entry.language.name === 'en')
-            ?.effect ?? ''
-        return { id, name, effect }
-      })
+      return abilitiesData
+        .map(({ id, name, effect_entries, slot }) => {
+          const effect =
+            effect_entries.find((entry) => entry.language.name === 'en')
+              ?.effect ?? ''
+          return { id, name, effect, slot }
+        })
+        .sort((a, b) => a.slot - b.slot)
     },
   })
 
